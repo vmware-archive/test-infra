fb4ca39336562c7c2d0f05c4e1e5a3652a80e62c
Tomas Pizarro <tomas@bitnami.com>
2017-08-30 10:24:44 +0000

    Update docker-image-test script

--- circle/docker-image-test.sh
+++ circle/docker-image-test.sh
@@ -14,22 +14,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+echo "======> Forked and edited script <======"
+
 CIRCLE_CI_FUNCTIONS_URL=${CIRCLE_CI_FUNCTIONS_URL:-https://raw.githubusercontent.com/bitnami/test-infra/master/circle/functions}
 source <(curl -sSL $CIRCLE_CI_FUNCTIONS_URL)
-
-echo "========== Forked script =========="
 
 docker_load_cache
 
 if [[ -n $RELEASE_SERIES_LIST ]]; then
   IFS=',' read -ra RELEASE_SERIES_ARRAY <<< "$RELEASE_SERIES_LIST"
   for RS in "${RELEASE_SERIES_ARRAY[@]}"; do
+    IFS=',' read -ra SUPPORTED_BASE_IMAGES_ARRAY <<< "$SUPPORTED_BASE_IMAGES"
     if [[ -n $IMAGE_TAG ]]; then
-      if [[ "$IMAGE_TAG" == "$RS"* ]]; then
-        docker_build $DOCKER_PROJECT/$IMAGE_NAME:$RS $RS || exit 1
-      fi
+        if [[ "$IMAGE_TAG" == "$RS"* ]]; then
+            for BI in "${SUPPORTED_BASE_IMAGES_ARRAY[@]}"; do
+                [[ $BI != "debian" ]] && TAG=$RS-$BI || TAG=$RS
+                docker_build $DOCKER_PROJECT/$IMAGE_NAME:$TAG $RS/$BI || exit 1
+            done
+        fi
     else
-      docker_build $DOCKER_PROJECT/$IMAGE_NAME:$RS $RS || exit 1
+        for BI in "${SUPPORTED_BASE_IMAGES_ARRAY[@]}"; do
+            [[ $BI != "debian" ]] && TAG=$RS-$BI || TAG=$RS
+            docker_build $DOCKER_PROJECT/$IMAGE_NAME:$TAG $RS/$BI || exit 1
+        done
     fi
   done
 else
