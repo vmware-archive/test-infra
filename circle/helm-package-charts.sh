b0d41f3b07c677f3f9eb5c075003a05d283e0276
tompizmor <tompizmor@gmail.com>
2018-04-05 12:22:31 +0200

    Add bitnami-library repo to resolve dependencies

--- circle/helm-package-charts.sh
+++ circle/helm-package-charts.sh
@@ -21,6 +21,7 @@ install_helm || exit 1
 # Adding bitnami chart repos to resolve dependencies while packaging the chart
 add_repo_to_helm bitnami-stable https://charts.bitnami.com/stable
 add_repo_to_helm bitnami-incubator https://charts.bitnami.com/incubator
+add_repo_to_helm bitnami-library https://charts.bitnami.com/library
 
 for chart_yaml in $(find $CIRCLE_WORKING_DIRECTORY -name Chart.yaml)
 do
