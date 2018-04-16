4a09b11668d95970ea65c1219e2a7d12eaae22e2
Juan Ariza Toledano <juan@bitnami.com>
2018-04-16 10:35:08 +0200

    Fix CircleCI

--- circle/docker-release-image.sh
+++ circle/docker-release-image.sh
@@ -107,7 +107,7 @@ if [[ -n $GCLOUD_PROJECT && -n $GCLOUD_SERVICE_KEY ]]; then
 fi
 
 if [[ -n $AZURE_PROJECT && -n $AZURE_PORTAL_PASS && -n $AZURE_PORTAL_USER ]]; then
-  azure_login || exit 1
+  az_login || exit 1
   for TAG in "${TAGS_TO_UPDATE[@]}"; do
     docker_build_and_push bitnamicatalog.azurecr.io/$AZURE_PROJECT/$IMAGE_NAME:$TAG $RELEASE_SERIES ${CACHE_TAG:+$DOCKER_PROJECT/$IMAGE_NAME:$CACHE_TAG} || exit 1
   done
